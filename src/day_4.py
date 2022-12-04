4b87f0cc97acb61bce0ca4d1f23abeaaed8589ab
Lucas Queiroz <lucas.queiroz@pagefreezer.com>
2022-12-04 02:26:24 -0300

    Day 4 - Part 2

--- src/day_4.py
+++ src/day_4.py
@@ -9,25 +9,26 @@ class Day4(BaseDay):
             self.run_part_2()
 
     def run_part_1(self):
-        self.result = self._get_overlapping_pairs()
+        self.result = self._get_overlapping_pairs(part=1)
 
     def run_part_2(self):
-        pass
+        self.result = self._get_overlapping_pairs(part=2)
 
-    def _get_overlapping_pairs(self) -> int:
+    def _get_overlapping_pairs(self, part: int) -> int:
         total = 0
         for line in self.input:
             first_pair, second_pair = line.replace("\n", "").split(",")
-            first_pair_start, first_pair_stop = first_pair.split("-")
-            second_pair_start, second_pair_stop = second_pair.split("-")
-            first_range = range(int(first_pair_start), int(first_pair_stop) + 1)
-            second_range = range(int(second_pair_start), int(second_pair_stop) + 1)
-            if self._ranges_overlap(first_range, second_range):
+            first_range, second_range = [self._get_range(first_pair), self._get_range(second_pair)]
+            if self._ranges_overlap(first_range, second_range, part==2):
                 total += 1
 
         return total
 
-    def _ranges_overlap(self, range_a: range, range_b: range) -> bool:
-        list_a = list(range_a)
-        list_b = list(range_b)
-        return set(list_a).issubset(list_b) or set(list_b).issubset(list_a)
+    def _get_range(self, pair: str) -> range:
+        pair_start, pair_stop = pair.split("-")
+        return range(int(pair_start), int(pair_stop) + 1)
+
+    def _ranges_overlap(self, range_a: range, range_b: range, intersection: bool = False) -> bool:
+        if intersection:
+            return len(set(range_a).intersection(range_b)) > 0
+        return set(range_a).issubset(range_b) or set(range_b).issubset(range_a)
--- tests/test_day_4.py
+++ tests/test_day_4.py
@@ -18,16 +18,16 @@ class TestDay4(BaseTest):
 
         self.print_result(day_4, part=1)
 
-    # def test_day_4_part_2_test_input(self):
-    #     test_input = self.get_test_input()
-    #     day_4_test = Day4(test_input)
+    def test_day_4_part_2_test_input(self):
+        test_input = self.get_test_input()
+        day_4_test = Day4(test_input)
 
-    #     result = day_4_test.get_result(part=2)
+        result = day_4_test.get_result(part=2)
 
-    #     # assert result == 0
+        assert result == 4
 
-    # def test_day_4_part_2(self):
-    #     real_input = self.get_input()
-    #     day_4 = Day4(real_input)
+    def test_day_4_part_2(self):
+        real_input = self.get_input()
+        day_4 = Day4(real_input)
 
-    #     self.print_result(day_4, part=2)
+        self.print_result(day_4, part=2)
